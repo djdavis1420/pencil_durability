bf0f151cca3934c5a0de6fed230346aabd26c764
Dylan Davis <djdavis1420@gmail.com>
2019-05-01 22:50:15 -0500

    Added & Tested Sharpen Method on Pencil Class

--- src/models/pencil.py
+++ src/models/pencil.py
@@ -4,3 +4,8 @@ class Pencil:
         self.remaining_durability = durability
         self.length = length
         self.eraser = eraser
+
+    def sharpen(self):
+        if self.length > 0:
+            self.remaining_durability = self.maximum_durability
+            self.length -= 1
--- test/models/test_pencil.py
+++ test/models/test_pencil.py
@@ -11,3 +11,21 @@ class TestPencil:
         assert self.pencil.remaining_durability == 500
         assert self.pencil.length == 20
         assert self.pencil.eraser == 50
+
+    def test_sharpen__should_reset_remaining_durability_to_maximum_durability_and_reduce_length_by_one(self):
+        self.pencil.remaining_durability = 5
+        self.pencil.length = 1
+
+        self.pencil.sharpen()
+
+        assert self.pencil.maximum_durability == 500
+        assert self.pencil.length == 0
+
+    def test_sharpen__should_not_reset_remaining_durability_to_maximum_durability_due_to_length_of_zero(self):
+        self.pencil.remaining_durability = 5
+        self.pencil.length = 0
+
+        self.pencil.sharpen()
+
+        assert self.pencil.remaining_durability == 5
+        assert self.pencil.length == 0
