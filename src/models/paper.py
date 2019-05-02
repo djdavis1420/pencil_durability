92010d6871407af9a074531a08d138d9089aba41
Dylan Davis <djdavis1420@gmail.com>
2019-05-01 23:34:38 -0500

    Added & Tested Write Method on Paper Class

--- src/models/paper.py
+++ src/models/paper.py
@@ -1,3 +1,6 @@
 class Paper:
     def __init__(self):
         self.text = ''
+
+    def write(self, character):
+        self.text += character
--- test/models/test_paper.py
+++ test/models/test_paper.py
@@ -8,3 +8,26 @@ class TestPaper:
 
     def test__should_create_new_instance_of_paper_with_text_property(self):
         assert self.paper.text == ''
+
+    def test_write__should_write_capital_a_to_unpopulated_text_property(self):
+        character = 'A'
+
+        self.paper.write(character)
+
+        assert self.paper.text == 'A'
+
+    def test_write__should_write_blank_space_to_populated_text_property(self):
+        self.paper.text = 'A'
+        character = ' '
+
+        self.paper.write(character)
+
+        assert self.paper.text == 'A '
+
+    def test_write__should_write_capital_z_to_populated_text_property(self):
+        self.paper.text = 'A '
+        character = 'Z'
+
+        self.paper.write(character)
+
+        assert self.paper.text == 'A Z'
